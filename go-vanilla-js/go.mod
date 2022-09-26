907407c32058437b4fb1d28db3ffd84199af6fa9
Alex Grinman <me@alexgr.in>
2022-09-25 20:38:37 -0400

    add go proj structure

---
+++ go-vanilla-js/go.mod
@@ -0,0 +1,5 @@
+module footprint-go-js
+
+go 1.19
+
+require github.com/go-chi/chi v1.5.4
---
+++ go-vanilla-js/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+const (
+	port string = ":3000"
+)
+
+func main() {
+	fmt.Println("hello world")
+}
