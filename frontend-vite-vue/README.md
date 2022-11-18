841be78ac6f6db2881458532637122b0e13e82cc
Rafael Michels Motta <rafael@onefootprint.com>
2022-11-18 10:49:05 -0300

    Added vue example

---
+++ frontend-vite-vue/README.md
@@ -0,0 +1,16 @@
+# Vue
+
+This examples shows how to use Footprint along with other frameworks, like Vue
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
---
+++ frontend-vite-vue/src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<script>
+import footprint from '@onefootprint/footprint-js';
+
+export default {
+  methods: {
+    handleOpen() {
+      const publicKey = 'ob_test_WNgSBRR7uxoT8JRDBBflgw';
+      const appearance = {
+        theme: 'light',
+        variables: {
+          fontFamily: '"SF UI Text", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+          linkColor: '#101010',
+          colorError: '#D14343',
+          buttonPrimaryBg: '#52BD95',
+          buttonPrimaryHoverBg: '#429777',
+          buttonPrimaryActiveBg: '#317159',
+          buttonPrimaryColor: '#FFF',
+          buttonBorderRadius: '4px',
+          linkButtonColor: '#52BD95',
+          labelColor: '#101840',
+          inputBorderRadius: '4px',
+          inputBorderWidth: '1px',
+          inputPlaceholderColor: '#B5B5B5',
+          inputColor: '#474d66',
+          inputBg: '#FFFFFF',
+          inputBorderColor: '#d8dae5',
+          inputFocusElevation: 'none',
+          inputErrorFocusElevation: 'box-shadow: 0 0 0 2px #d6e0ff;',
+          inputHoverBorderColor: '#ADC2FF',
+          hintColor: '#696f8c',
+        }
+      };
+
+      footprint.show({
+        appearance,
+        publicKey,
+       })
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="card">
+    <button type="button" @click="handleOpen()">
+      Verify with Footprint
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
