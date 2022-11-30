d09be6e99538b5af6983ff63b35c81fb28c56938
Rafael Michels Motta <rafael@onefootprint.com>
2022-11-30 13:16:30 -0300

    Use createButton for vite (#8)

--- frontend-vite-vue/src/components/HelloWorld.vue
+++ frontend-vite-vue/src/components/HelloWorld.vue
@@ -2,37 +2,15 @@
 import footprint from '@onefootprint/footprint-js';
 
 export default {
+  mounted() {
+    const container = document.getElementById('footprint-container');
+    const button = footprint.createButton(container);
+    button.addEventListener('click', this.handleOpen)
+  },
   methods: {
     handleOpen() {
-      const publicKey = 'ob_test_WNgSBRR7uxoT8JRDBBflgw';
-      const appearance = {
-        theme: 'light',
-        variables: {
-          fontFamily: '"SF UI Text", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-          linkColor: '#101010',
-          colorError: '#D14343',
-          buttonPrimaryBg: '#52BD95',
-          buttonPrimaryHoverBg: '#429777',
-          buttonPrimaryActiveBg: '#317159',
-          buttonPrimaryColor: '#FFF',
-          buttonBorderRadius: '4px',
-          linkButtonColor: '#52BD95',
-          labelColor: '#101840',
-          inputBorderRadius: '4px',
-          inputBorderWidth: '1px',
-          inputPlaceholderColor: '#B5B5B5',
-          inputColor: '#474d66',
-          inputBg: '#FFFFFF',
-          inputBorderColor: '#d8dae5',
-          inputFocusElevation: 'none',
-          inputErrorFocusElevation: 'box-shadow: 0 0 0 2px #d6e0ff;',
-          inputHoverBorderColor: '#ADC2FF',
-          hintColor: '#696f8c',
-        },
-      };
       footprint.show({
-        appearance,
-        publicKey,
+        publicKey: 'ob_test_WNgSBRR7uxoT8JRDBBflgw',
       });
     }
   }
@@ -41,14 +19,7 @@ export default {
 
 <template>
   <div class="card">
-    <button type="button" @click="handleOpen()">
-      Verify with Footprint
-    </button>
+    <div id="footprint-container"></div>
   </div>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
