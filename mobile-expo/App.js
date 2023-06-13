c2e31b615a3334691876ae71835faaba993d19f6
Rafael Michels Motta <rafael@onefootprint.com>
2023-06-12 22:07:37 -0300

    Expo example

---
+++ mobile-expo/App.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { Button, View, StyleSheet } from 'react-native';
+import footprint from '@onefootprint/footprint-expo'
+
+export default function App() {
+  const handleButtonPress = async () => {
+    footprint.open({
+      publicKey: 'ob_test_ilBQgKtvrgQrQNV8U2rb7e',
+      onCompleted: (validationToken) => {
+        console.log(validationToken);
+      },
+      onCanceled: () => {
+        console.log('canceled');
+      },
+      userData: {
+        'id.email': 'jane.doe@acme.com',
+      },
+      appearance,
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button title="Open WebBrowser" onPress={handleButtonPress} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const appearance = {
+  fontSrc: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap',
+  variables: {
+    fontFamily: '"Inter"',
+    linkColor: '#101010',
+    colorError: '#E33D19',
+
+    buttonPrimaryBg: '#315E4C',
+    buttonPrimaryHoverBg: '#46866c',
+    buttonPrimaryColor: '#FFF',
+    buttonBorderRadius: '70px',
+
+    linkButtonColor: '#315E4C',
+
+    labelColor: '#101010',
+    labelFont: '600 15px/18px "Inter"',
+
+    inputBorderRadius: '8px',
+    inputBorderWidth: '1px',
+    inputFont: '500 15px/21.42px "Inter"',
+    inputHeight: '50px',
+    inputPlaceholderColor: '#B5B5B5',
+    inputColor: '#101010',
+    inputBg: '#FFFFFF',
+    inputBorderColor: '#B5B5B5',
+    inputHoverBorderColor: '#707070',
+    inputFocusBorderColor: '#707070',
+    inputFocusElevation: 'none',
+    inputErrorFocusElevation: 'none',
+    hintColor: '#101010',
+    hintFont: '400 13px/20px "Inter"',
+
+    dropdownBorderColor: '#B5B5B5',
+    dropdownBorderRadius: '8px',
+    dropdownElevation: 'unset',
+    dropdownBg: '#FFF',
+    dropdownHoverBg: '#F9F9F9',
+  },
+  rules: {
+    button: {
+      transition: 'all .2s linear',
+    },
+  }
+}
