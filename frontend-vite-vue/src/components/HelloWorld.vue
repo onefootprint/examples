d7d130926c4640180ade1d69434ba3ef38a38e1c
Rafael Michels Motta <rafael@onefootprint.com>
2023-09-13 20:56:07 -0300

    Update frontend-vite-vue

--- frontend-vite-vue/src/components/HelloWorld.vue
+++ frontend-vite-vue/src/components/HelloWorld.vue
@@ -1,49 +1,31 @@
 <script>
-import footprintComponent from '@onefootprint/footprint-components-js';
 import footprint from '@onefootprint/footprint-js';
-import '@onefootprint/footprint-components-js/dist/footprint-components-js.css';
+
 
 export default {
   mounted() {
-    const container = document.getElementById('footprint-container');
-    const button = footprint.createButton(container);
-    button.addEventListener('click', this.handleOpen)
-
-    footprintComponent.render({
-      kind: 'secure-form',
-      containerId: 'footprint-secure-form',
-      props: {
-        authToken: 'tok_joXzzB0kIVW0fMCB7RWPAHWt8itWdFWpit', // auth token generated using the Secret API Key on step 5
-        cardAlias: 'primary', // this should match the cardAliases used while generating the auth token
-        variant: 'card',
-        onSave: () => {
-          console.log("saved");
-        },
-        onCancel: () => {
-          console.log("canceled");
-        },
-        onClose: () => {
-          console.log("closed");
-        }
-      },
-    });
+    const button = document.getElementById('verify-button');
+    button.addEventListener('click', this.handleOpen);
   },
   methods: {
     handleOpen() {
-      footprint.open({
-        publicKey: 'ob_test_WNgSBRR7uxoT8JRDBBflgw',
+      const publicKey = 'ob_test_WNgSBRR7uxoT8JRDBBflgw';
+      const component = footprint.init({
+        kind: 'verify',
+        publicKey,
+        onComplete: (validationToken) => {
+          console.log(validationToken)
+        },
       });
+      component.render();
     }
   }
 }
 </script>
 
 <template>
-  <div class="card">
-    <div class="container">
-      <div id="footprint-container"></div>
-      <div id="footprint-secure-form" class="form"></div>
-    </div>
-  </div>
+  <main>
+    <button id="verify-button" type="button">Verify with Footprint</button>
+  </main>
 </template>
 
