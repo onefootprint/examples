a4cf970a08c4cb17fbf45e45486a36f3ef168935
Rafael Michels Motta <rafael@onefootprint.com>
2023-09-13 20:26:19 -0300

    Update docs

--- frontend-react-typescript-next/pages/index.tsx
+++ frontend-react-typescript-next/pages/index.tsx
@@ -3,8 +3,7 @@ import "@onefootprint/footprint-js/dist/footprint-js.css";
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { FootprintButton } from "@onefootprint/footprint-react";
-import { FootprintSecureForm } from "@onefootprint/footprint-components-react";
+import { FootprintVerifyButton } from "@onefootprint/footprint-react";
 
 const Home: NextPage = () => {
   return (
@@ -17,23 +16,16 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div style={styles.container}>
-          <FootprintButton
+        <div className={styles.container}>
+          <FootprintVerifyButton
             publicKey="ob_test_WNgSBRR7uxoT8JRDBBflgw"
-            onCompleted={(validationToken) => {
+            onComplete={(validationToken) => {
               console.log("on completed", validationToken);
             }}
-            onCanceled={() => {
+            onCancel={() => {
               console.log("user canceled!");
             }}
           />
-          <div style={styles.form}>
-            <FootprintSecureForm 
-              authToken='tok_XYZ'
-              cardAlias='primary'
-              variant='card'
-            />
-          </div>
         </div>
       </main>
     </div>
