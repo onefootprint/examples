9b88b22790bfe610ca0bccad1e94543e95888534
Belce Dogru <belce.dogru@gmail.com>
2023-10-23 18:41:14 -0400

    Add frontend-angular (#47)

---
+++ frontend-angular/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import footprint, { FootprintComponentKind } from '@onefootprint/footprint-js';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // TODO: replace the auth token below with a server generated token
+  public handleClick() {
+    const component = footprint.init({
+      kind: FootprintComponentKind.Verify,
+      authToken: 'tok_1232132',
+      onComplete: (validationToken: string) => {
+        console.log('onComplete ', validationToken)
+      },
+      onCancel: () => {
+        console.log('onCancel')
+      },
+      onClose: () => {
+        console.log('onClose')
+      },
+    });
+
+    component.render();
+  }
+}
