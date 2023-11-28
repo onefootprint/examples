cfa9b1012c6a2f9aa7547e362deeee8b27e66f79
Belce Dogru <belce.dogru@gmail.com>
2023-11-28 12:46:30 -0800

    button and deps

--- idv/frontend-rails/config/importmap.rb
+++ idv/frontend-rails/config/importmap.rb
@@ -1,4 +1,5 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@onefootprint/footprint-js", to: "https://ga.jspm.io/npm:@onefootprint/footprint-js@3.8.0/dist/footprint-js.js"
+pin "@onefootprint/postmate", to: "https://ga.jspm.io/npm:@onefootprint/postmate@2.1.0/build/postmate.es.js"
