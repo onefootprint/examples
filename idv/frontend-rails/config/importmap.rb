050c0ed9d2406286eb607be45df935ea9ead4ff1
Belce Dogru <belce.dogru@gmail.com>
2023-11-30 15:54:38 -0800

    Fix rails integration importmap (#53)

--- idv/frontend-rails/config/importmap.rb
+++ idv/frontend-rails/config/importmap.rb
@@ -2,7 +2,7 @@
 
 pin "application", preload: true
 pin "@onefootprint/footprint-js", to: "https://ga.jspm.io/npm:@onefootprint/footprint-js@3.8.0/dist/footprint-js.js"
-pin "@onefootprint/postmate", to: "https://ga.jspm.io/npm:@onefootprint/postmate@2.1.0/build/postmate.es.js"
+pin "@onefootprint/postmate", to: "https://ga.jspm.io/npm:@onefootprint/postmate@2.1.0/build/postmate.js"
 
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
