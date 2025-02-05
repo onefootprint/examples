d18f030b5d0072517f6e46d7cf6ba2726104dc60
rodrigopagnuzzi <rodrigo@onefootprint.com>
2025-02-05 13:54:12 -0300

    Modify the React example to include the inline variant

--- idv/frontend-react-typescript-next/pages/index.tsx
+++ idv/frontend-react-typescript-next/pages/index.tsx
@@ -6,33 +6,36 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  const handleOpen = () => {
-    const component = footprint.init({
-      kind: 'verify',
-      publicKey: 'pb_test_5OJt1d7gDASCzgv7pZpLuB',
-      onComplete: (validationToken: string) => {
-        console.log(validationToken);
-      },
-    });
-    component.render();
-  }
+	const handleOpen = () => {
+		const component = footprint.init({
+			kind: "verify",
+			variant: "inline",
+			containerId: "footprint-container",
+			publicKey: "pb_test_5OJt1d7gDASCzgv7pZpLuB",
+			onComplete: (validationToken: string) => {
+				console.log(validationToken);
+			},
+		});
+		component.render();
+	};
 
-  return (
-    <>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#000000" />
-        <title>Footprint | React + Typescript + Next.js</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <div className={styles.container}>
-          <button type="button" onClick={handleOpen}>Verify</button>
-        </div>
-      </main>
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta name="theme-color" content="#000000" />
+				<title>Footprint | React + Typescript + Next.js</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main className={styles.main}>
+				<div id="footprint-container" className={styles.container} />
+				<button type="button" onClick={handleOpen}>
+					Verify
+				</button>
+			</main>
+		</>
+	);
 };
 
 export default Home;
