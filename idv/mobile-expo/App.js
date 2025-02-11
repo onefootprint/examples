5133f767ad2b27354154d13ae0358048f97b626e
Rafael Michels Motta <rafael@onefootprint.com>
2025-02-11 11:11:13 -0300

    Update example code

--- idv/mobile-expo/App.js
+++ idv/mobile-expo/App.js
@@ -1,26 +1,20 @@
-import React from "react";
-import { Button, View, StyleSheet } from "react-native";
-import footprint from "@onefootprint/footprint-expo";
+import { Button, View, StyleSheet } from 'react-native';
+import footprint from '@onefootprint/footprint-expo';
 
 export default function App() {
-  const handleButtonPress = async () => {
+  const handleButtonPress = () => {
     const component = footprint.init({
-      publicKey: "ob_test_ilBQgKtvrgQrQNV8U2rb7e",
-      onComplete: (validationToken) => {
+      publicKey: 'ob_test_ilBQgKtvrgQrQNV8U2rb7e',
+      onComplete: validationToken => {
         alert(validationToken);
       },
       onCancel: () => {
-        console.log("canceled");
+        console.log('canceled');
       },
-      onError: (error) => {
-        console.error(error)
+      onError: error => {
+        console.error(error);
       },
-      bootstrapData: {
-        "id.email": "jane.doe@acme.com",
-        "id.phone_number": "+15555550100",
-      },
-      appearance,
-      l10n: { locale: "en-US" },
+      l10n: { locale: 'en-US' },
     });
     component.render();
   };
@@ -35,55 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
-
-const appearance = {
-  fontSrc:
-    "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap",
-  variables: {
-    fontFamily: '"Inter"',
-    linkColor: "#101010",
-    colorError: "#E33D19",
-
-    buttonPrimaryBg: "#315E4C",
-    buttonPrimaryHoverBg: "#46866c",
-    buttonPrimaryColor: "#FFF",
-    buttonBorderRadius: "70px",
-
-    linkButtonColor: "#315E4C",
-
-    labelColor: "#101010",
-    labelFont: '600 15px/18px "Inter"',
-
-    inputBorderRadius: "8px",
-    inputBorderWidth: "1px",
-    inputFont: '500 15px/21.42px "Inter"',
-    inputHeight: "50px",
-    inputPlaceholderColor: "#B5B5B5",
-    inputColor: "#101010",
-    inputBg: "#FFFFFF",
-    inputBorderColor: "#B5B5B5",
-    inputHoverBorderColor: "#707070",
-    inputFocusBorderColor: "#707070",
-    inputFocusElevation: "none",
-    inputErrorFocusElevation: "none",
-    hintColor: "#101010",
-    hintFont: '400 13px/20px "Inter"',
-
-    dropdownBorderColor: "#B5B5B5",
-    dropdownBorderRadius: "8px",
-    dropdownElevation: "unset",
-    dropdownBg: "#FFF",
-    dropdownHoverBg: "#F9F9F9",
-  },
-  rules: {
-    button: {
-      transition: "all .2s linear",
-    },
-  },
-};
-
---
+++ idv/mobile-expo/WithBootstrapAndAppearance.js
@@ -0,0 +1,86 @@
+import { Button, View, StyleSheet } from 'react-native';
+import footprint from '@onefootprint/footprint-expo';
+
+export default function App() {
+  const handleButtonPress = () => {
+    const component = footprint.init({
+      publicKey: 'ob_test_ilBQgKtvrgQrQNV8U2rb7e',
+      onComplete: validationToken => {
+        alert(validationToken);
+      },
+      onCancel: () => {
+        console.log('canceled');
+      },
+      onError: error => {
+        console.error(error);
+      },
+      bootstrapData: {
+        'id.email': 'jane.doe@acme.com',
+        'id.phone_number': '+15555550100',
+      },
+      appearance,
+      l10n: { locale: 'en-US' },
+    });
+    component.render();
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button title="Verify with Footprint" onPress={handleButtonPress} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const appearance = {
+  fontSrc: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap',
+  variables: {
+    fontFamily: '"Inter"',
+    linkColor: '#101010',
+    colorError: '#E33D19',
+
+    buttonPrimaryBg: '#315E4C',
+    buttonPrimaryHoverBg: '#46866c',
+    buttonPrimaryColor: '#FFF',
+    buttonBorderRadius: '70px',
+
+    linkButtonColor: '#315E4C',
+
+    labelColor: '#101010',
+    labelFont: '600 15px/18px "Inter"',
+
+    inputBorderRadius: '8px',
+    inputBorderWidth: '1px',
+    inputFont: '500 15px/21.42px "Inter"',
+    inputHeight: '50px',
+    inputPlaceholderColor: '#B5B5B5',
+    inputColor: '#101010',
+    inputBg: '#FFFFFF',
+    inputBorderColor: '#B5B5B5',
+    inputHoverBorderColor: '#707070',
+    inputFocusBorderColor: '#707070',
+    inputFocusElevation: 'none',
+    inputErrorFocusElevation: 'none',
+    hintColor: '#101010',
+    hintFont: '400 13px/20px "Inter"',
+
+    dropdownBorderColor: '#B5B5B5',
+    dropdownBorderRadius: '8px',
+    dropdownElevation: 'unset',
+    dropdownBg: '#FFF',
+    dropdownHoverBg: '#F9F9F9',
+  },
+  rules: {
+    button: {
+      transition: 'all .2s linear',
+    },
+  },
+};
